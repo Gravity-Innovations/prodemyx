ad33ffd8ab17ec3eb60c77260c696dc631db386e
Jason-teamGravity <129653238+Jason-teamGravity@users.noreply.github.com>
2025-12-25 09:28:38 +0000

    Remove backup directory

--- .backup/dashboard/src/pages/CourseDetails.jsx
+++
@@ -1,8 +0,0 @@
-export default function CourseDetails() {
-  return (
-    <div style={{ padding: "40px" }}>
-      <h1>Course Details Page</h1>
-      <p>This is where detailed course info will appear.</p>
-    </div>
-  );
-}
