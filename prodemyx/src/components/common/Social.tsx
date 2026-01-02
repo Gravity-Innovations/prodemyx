1aac777918184a394ac8fda002d7b1a827de013c
mariyagravity <mariya@teamgravity.in>
2026-01-02 12:09:13 +0530

    whatsapp

--- prodemyx/src/components/common/Social.tsx
+++ prodemyx/src/components/common/Social.tsx
@@ -1,36 +1,81 @@
-import { Link } from "react-router-dom"
-import InjectableSvg from "../../hooks/InjectableSvg"
+import InjectableSvg from "../../hooks/InjectableSvg";
+
+const WHATSAPP_NUMBER = "918089438099";
 
 const Social = () => {
-   return (
-      <>
-         <li>
-            <Link to="https://www.facebook.com/" target="_blank">
-               <InjectableSvg src="/assets/img/icons/facebook.svg" alt="img" className="injectable" />
-            </Link>
-         </li>
-         <li>
-            <Link to="https://www.facebook.com/" target="_blank">
-               <InjectableSvg src="/assets/img/icons/twitter.svg" alt="img" className="injectable" />
-            </Link>
-         </li>
-         <li>
-            <Link to="https://www.facebook.com/" target="_blank">
-               <InjectableSvg src="/assets/img/icons/whatsapp.svg" alt="img" className="injectable" />
-            </Link>
-         </li>
-         <li>
-            <Link to="https://www.facebook.com/" target="_blank">
-               <InjectableSvg src="/assets/img/icons/instagram.svg" alt="img" className="injectable" />
-            </Link>
-         </li>
-         <li>
-            <Link to="https://www.facebook.com/" target="_blank">
-               <InjectableSvg src="/assets/img/icons/youtube.svg" alt="img" className="injectable" />
-            </Link>
-         </li>
-      </>
-   )
-}
+  return (
+    <>
+      <li>
+        <a
+          href="https://www.facebook.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <InjectableSvg
+            src="/assets/img/icons/facebook.svg"
+            alt="Facebook"
+            className="injectable"
+          />
+        </a>
+      </li>
 
-export default Social
+      <li>
+        <a
+          href="https://twitter.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <InjectableSvg
+            src="/assets/img/icons/twitter.svg"
+            alt="Twitter"
+            className="injectable"
+          />
+        </a>
+      </li>
+
+      <li>
+        <a
+          href={`https://wa.me/${WHATSAPP_NUMBER}`}
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <InjectableSvg
+            src="/assets/img/icons/whatsapp.svg"
+            alt="WhatsApp"
+            className="injectable"
+          />
+        </a>
+      </li>
+
+      <li>
+        <a
+          href="https://www.instagram.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <InjectableSvg
+            src="/assets/img/icons/instagram.svg"
+            alt="Instagram"
+            className="injectable"
+          />
+        </a>
+      </li>
+
+      <li>
+        <a
+          href="https://www.youtube.com/"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          <InjectableSvg
+            src="/assets/img/icons/youtube.svg"
+            alt="YouTube"
+            className="injectable"
+          />
+        </a>
+      </li>
+    </>
+  );
+};
+
+export default Social;
